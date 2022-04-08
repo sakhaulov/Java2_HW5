ae76fb9460bc940786dec35b27d16be8e6d659e9
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2022-04-09 01:19:08 +0500

    Cleaned up

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         //Тестовые данные
         File file = new File("file.csv");
